484e12198d3edbfd2bf1df47a2133bc66e94237c
Semir Vrana <semir.vrana@gmx.de>
2018-09-30 10:38:50 +0200

    Fix QLayout warning

--- src/mines.hh
+++ src/mines.hh
@@ -21,29 +21,29 @@ class mines : public QMainWindow {
 
 public:
   mines() : QMainWindow(nullptr) {
-    int rows        = 16;
-    int cols        = 30;
-    int num_bombs   = 99;
-    _central_widget = new QWidget(this);
-    this->setCentralWidget(_central_widget);
-    this->setFixedSize(cols * 22, rows * 23);
+    int rows      = 16;
+    int cols      = 30;
+    int num_bombs = 99;
 
-    _layout     = new QVBoxLayout(this);
     _board      = new gameboard(rows, cols, num_bombs, this);
     _timer      = new timer(this);
     _highscore  = new highscore;
     _bomb_count = new bomb_count(num_bombs, this);
 
     auto top_bar    = new QWidget(this);
-    auto top_layout = new QHBoxLayout(this);
+    auto top_layout = new QHBoxLayout(top_bar);
     top_bar->setLayout(top_layout);
     top_layout->addWidget(_timer);
     top_layout->addWidget(_bomb_count);
 
+    _central_widget = new QWidget(this);
+    _layout         = new QVBoxLayout(_central_widget);
     _layout->addWidget(top_bar);
     _layout->addWidget(_board);
-
     _central_widget->setLayout(_layout);
+
+    this->setCentralWidget(_central_widget);
+    this->setFixedSize(cols * 22, rows * 23);
 
     connect(_board, &gameboard::game_started, _timer, &timer::start);
     connect(_board, &gameboard::game_done, _timer, &timer::stop);
