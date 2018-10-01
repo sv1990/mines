ea8d7c2196a90cdf58a679a4333a02acf3fa6500
Semir Vrana <semir.vrana@gmx.de>
2018-10-01 09:16:13 +0200

    Change UI and add button to show highscore

--- src/mines.cc
+++ src/mines.cc
@@ -13,13 +13,10 @@ mines::mines() noexcept : QMainWindow(nullptr) {
   _highscore  = new highscore;
   _bomb_count = new bomb_count(num_bombs, this);
 
-  auto top_bar        = new QWidget(this);
-  auto top_layout     = new QHBoxLayout(top_bar);
-  auto restart_button = new QPushButton(this);
-  restart_button->setText("Restart");
+  auto top_bar    = new QWidget(this);
+  auto top_layout = new QHBoxLayout(top_bar);
   top_bar->setLayout(top_layout);
   top_layout->addWidget(_timer);
-  top_layout->addWidget(restart_button);
   top_layout->addWidget(_bomb_count);
 
   _central_widget = new QWidget(this);
@@ -28,21 +25,34 @@ mines::mines() noexcept : QMainWindow(nullptr) {
   _layout->addWidget(_board);
   _central_widget->setLayout(_layout);
 
+  auto bottom_bar       = new QWidget(this);
+  auto bottom_layout    = new QHBoxLayout(bottom_bar);
+  auto highscore_button = new QPushButton(this);
+  highscore_button->setText("Show Highscore");
+  bottom_layout->addWidget(highscore_button);
+  auto restart_button = new QPushButton(this);
+  restart_button->setText("Restart");
+  bottom_layout->addWidget(restart_button);
+  _layout->addWidget(bottom_bar);
+
   this->setCentralWidget(_central_widget);
-  this->setFixedSize(cols * 22, rows * 23);
+  this->setFixedSize(cols * 20, rows * 26);
 
   connect(_board, &gameboard::game_started, _timer, &timer::start);
   connect(_board, &gameboard::game_done, _timer, &timer::stop);
   connect(_board, &gameboard::game_done, this, &mines::show_highscore);
   connect(_board, &gameboard::marks_changed, _bomb_count,
           &bomb_count::count_changed);
+  connect(highscore_button, &QPushButton::clicked, this,
+          &mines::show_highscore);
   connect(restart_button, &QPushButton::clicked, _board, &gameboard::reset);
   connect(restart_button, &QPushButton::clicked, _timer, &timer::reset);
-  connect(_board, &gameboard::resetted_bombs, _bomb_count, &bomb_count::restart);
+  connect(_board, &gameboard::resetted_bombs, _bomb_count,
+          &bomb_count::restart);
 }
 
 void mines::show_highscore() noexcept {
-  if (!_board->lost()) {
+  if (_board->is_done() && !_board->lost()) {
     _highscore->add(_timer->seconds());
   }
   _highscore->show();
