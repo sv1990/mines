23debef6085211cc38e2a00f6ffece3455fe0152
Semir Vrana <semir.vrana@gmx.de>
2018-10-05 21:54:06 +0200

    Hardcode width and height for each difficulty

--- src/difficulty.hh
+++ src/difficulty.hh
@@ -5,6 +5,8 @@ struct difficulty {
   int rows;
   int cols;
   int bombs;
+  int width;
+  int height;
 };
 
 #endif // MINES_SRC_DIFFICULTY_HH_1538765447834410874_
--- src/mines.cc
+++ src/mines.cc
@@ -5,13 +5,16 @@
 #include <QSpinBox>
 
 const std::vector<difficulty> mines::_difficulties{
-    {8, 8, 10},   // Easy
-    {16, 16, 40}, // Intermediate
-    {19, 30, 99}, // Hard
+    {8, 8, 10, 200, 280},   // Easy
+    {16, 16, 40, 390, 410}, // Intermediate
+    {19, 30, 99, 600, 470}, // Hard
 };
 
 mines::mines() noexcept : QMainWindow(nullptr) {
-  auto [rows, cols, num_bombs] = _difficulties[2];
+  const int initial_difficulty_index = 2;
+
+  auto [rows, cols, num_bombs, width, height] =
+      _difficulties[initial_difficulty_index];
 
   _board      = new gameboard(rows, cols, num_bombs, this);
   _timer      = new timer(this);
@@ -25,7 +28,7 @@ mines::mines() noexcept : QMainWindow(nullptr) {
   _difficulty_box->addItem("Easy");
   _difficulty_box->addItem("Intermediate");
   _difficulty_box->addItem("Hard");
-  _difficulty_box->setCurrentIndex(2);
+  _difficulty_box->setCurrentIndex(initial_difficulty_index);
 
   _highscore = new highscore(_difficulty_box->currentText().toStdString());
 
@@ -51,7 +54,7 @@ mines::mines() noexcept : QMainWindow(nullptr) {
   _layout->addWidget(bottom_bar);
 
   this->setCentralWidget(_central_widget);
-  // this->setFixedSize(cols * 20, rows * 26);
+  this->setFixedSize(width, height);
 
   connect(_board, &gameboard::game_started, _timer, &timer::start);
   connect(_board, &gameboard::game_done, _timer, &timer::stop);
@@ -77,19 +80,20 @@ void mines::add_highscore() noexcept {
 }
 
 void mines::change_difficulty() {
-  auto [rows, cols, num_bombs] =
+  auto [rows, cols, num_bombs, width, height] =
       _difficulties[static_cast<std::size_t>(_difficulty_box->currentIndex())];
   set_rows(rows);
   set_cols(cols);
   set_bombs(num_bombs);
   _highscore->change_difficulty(_difficulty_box->currentText().toStdString());
+  this->setFixedSize(width, height);
   restart();
 }
 
 void mines::restart() {
   _board->reset();
   _timer->reset();
-  auto [rows, cols, bombs] =
+  auto [rows, cols, bombs, widht, height] =
       _difficulties[static_cast<std::size_t>(_difficulty_box->currentIndex())];
   _bomb_count->restart(bombs);
   connect(_board, &gameboard::resetted_bombs, _bomb_count,
