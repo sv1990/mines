dade6f09f28616e879715c8a069903083a97e23f
Semir Vrana <semir.vrana@gmx.de>
2018-11-18 16:51:09 +0100

    Fix main

--- src/main.cc
+++ src/main.cc
@@ -4,16 +4,18 @@
 #include <QMessageBox>
 
 int main(int argc, char* argv[]) {
-  QApplication a(argc, argv);
   try {
+    QApplication a(argc, argv);
     mines m;
     m.show();
+    return a.exec();
   }
   catch (std::exception& e) {
+    QApplication a(argc, argv);
     QMessageBox exception_msg_;
     exception_msg_.setText("An exception occurred:");
     exception_msg_.setInformativeText(e.what());
     exception_msg_.exec();
+    return a.exec();
   }
-  return a.exec();
 }
