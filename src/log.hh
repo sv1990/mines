26882e4f8523233a0d2373c40e577b0343a8915b
Semir Vrana <semir.vrana@gmx.de>
2019-12-06 08:31:54 +0100

    Remove unused logger

--- src/log.hh
+++
@@ -1,8 +0,0 @@
-#ifndef MINES_SRC_LOG_HH_1575568213189068818_
-#define MINES_SRC_LOG_HH_1575568213189068818_
-
-#include <spdlog/spdlog.h>
-
-spdlog::logger& logger();
-
-#endif // MINES_SRC_LOG_HH_1575568213189068818_
