9e7a8a9bc645aaebd24e5c3d75402725e0ea4aad
Semir Vrana <semir.vrana@gmx.de>
2019-12-20 17:11:51 +0100

    Add precompiled headers

---
+++ src/pch.hh
@@ -0,0 +1,12 @@
+#ifndef MINES_SRC_PCH_HH_1576857713736225858_
+#define MINES_SRC_PCH_HH_1576857713736225858_
+
+#include <algorithm>
+#include <chrono>
+#include <filesystem>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <functional>
+
+#endif // MINES_SRC_PCH_HH_1576857713736225858_
