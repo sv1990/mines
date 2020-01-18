4e171777aca8b1fc490ecb6d398d48a6a049d1e3
Semir Vrana <semir.vrana@gmx.de>
2020-01-18 18:49:04 +0100

    make_qobject works only with types deriving from QObject

--- src/util/make_qobject.hh
+++ src/util/make_qobject.hh
@@ -1,10 +1,14 @@
 #ifndef MINES_SRC_UTIL_MAKE_QOBJECT_HH_1579011197024269193_
 #define MINES_SRC_UTIL_MAKE_QOBJECT_HH_1579011197024269193_
 
+#include <type_traits>
 #include <utility>
 
+#include <QObject>
+
 namespace util {
-template <typename T, typename... Ts>
+template <typename T, typename... Ts,
+          std::enable_if_t<std::is_base_of_v<QObject, T>>* = nullptr>
 [[nodiscard]] T* make_qobject(Ts&&... xs) {
   return new T(std::forward<Ts>(xs)...);
 }
